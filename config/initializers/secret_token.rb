8e9e4cedd72ab975a95f1942a0d9046365113b4f
Philip Arndt <parndt@gmail.com>
2011-09-07 18:23:09 +1200

    Proof of 'bundler bug'?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BundlerError::Application.config.secret_token = '16d1e54e8680d174e907910452786a126c242ad77634f2dbfce703f4914828735086b4c84ed197b271a1a4db7e2a446d50bf213b533b71e80198ac8d5c787f80'
